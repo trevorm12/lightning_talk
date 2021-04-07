d3041d115d961b8943070f2d42bc7c87f5fecb00
renee-cole <67065956+renee-cole@users.noreply.github.com>
2021-04-06 20:57:41 -0500

    Update

--- main.py
+++ main.py
@@ -2,50 +2,55 @@
 
 import simpy
 import random
+import numpy as np
 
-NUM_NURSES = 50
+NUM_NURSES = 25
 VACCINE_TIME = 5
 PAPER_TIME = 15
 CHECK_TIME = 10
 WAIT_TIME = 15
 APPT_INTER = 5
 INIT_NUM_PEOPLE = 25
-SIM_TIME = 120
+SIM_TIME = 1000
+TIME_LIST = []
 
 class vaccineCenter(object):
     def __init__(self, env, num_nurses, vaccine_time, paper_time, check_time, wait_time):
-        self.env = env
         self.nurse = simpy.Resource(env, num_nurses)
+        self.env = env
         self.vaccination_time = vaccine_time
         self.paperwork_time = paper_time
         self.reaction_check_time = check_time
         self.waiting_time = wait_time
 
-    def wait_in_line(self, person):
+    def wait_in_line(self):
         yield self.env.timeout(random.randint(self.waiting_time-2, self.waiting_time+3))
 
-    def fill_paperwork(self, person):
+    def fill_paperwork(self):
         yield self.env.timeout(random.randint(self.paperwork_time - 2, self.paperwork_time + 3))
 
-    def get_microchip_implanted(self, person):
+    def get_vaccinated(self):
         yield self.env.timeout(random.randint(self.vaccination_time - 2, self.paperwork_time + 3))
 
-    def wait_for_reaction(self, person):
+    def wait_for_reaction(self):
         yield self.env.timeout(random.randint(self.reaction_check_time - 2, self.reaction_check_time + 3))
 
 
 def person(env, name, vc):
-    print('%s arrives at the vaccine center at %.2f.' % (name, env.now))
+    # print('%s arrives at the vaccine center at %.2f.' % (name, env.now))
+    init_time = env.now
     with vc.nurse.request() as request:
         yield request
+        # print('%s enters the vaccine center at %.2f.' % (name, env.now))
+        yield env.process(vc.wait_in_line())
+        yield env.process(vc.fill_paperwork())
+        yield env.process(vc.get_vaccinated())
+        yield env.process(vc.wait_for_reaction())
 
-        print('%s enters the vaccine center at %.2f.' % (name, env.now))
-        yield env.process(vc.wait_in_line(name))
-        yield env.process(vc.fill_paperwork(name))
-        yield env.process(vc.get_microchip_implanted(name))
-        yield env.process(vc.wait_for_reaction(name))
-
-        print('%s leaves the vaccine center at %.2f.' % (name, env.now))
+        # print('%s leaves the vaccine center at %.2f.' % (name, env.now))
+        fin_time = env.now
+    print('%s spent %.2f to get their shot.' % (name, fin_time-init_time))
+    TIME_LIST.append(fin_time-init_time)
 
 
 def setup(env, num_nurses, vaccine_time, paper_time, check_time, wait_time, appointment_inter):
@@ -54,11 +59,11 @@ def setup(env, num_nurses, vaccine_time, paper_time, check_time, wait_time, appo
     # Create the vaccine center
     center = vaccineCenter(env, num_nurses, vaccine_time, paper_time, check_time, wait_time)
 
-    # Create 4 initial cars
+    # Create initial patients
     for i in range(INIT_NUM_PEOPLE):
         env.process(person(env, 'Person %d' % i, center))
 
-    # Create more cars while the simulation is running
+    # Create more patients while the simulation is running
     while True:
         yield env.timeout(random.randint(appointment_inter - 2, appointment_inter + 2))
         i += 1
@@ -69,3 +74,4 @@ env = simpy.Environment()
 env.process(setup(env, NUM_NURSES, VACCINE_TIME, PAPER_TIME, CHECK_TIME, WAIT_TIME, APPT_INTER))
 
 env.run(until=SIM_TIME)
+print('It took an average of %.2f for each person to get their shot.' % np.mean(TIME_LIST))
